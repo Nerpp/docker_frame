81fee4f709b1acb074c1e86b4463adb4412b79e6
Nerpp <regiaurelien@gmail.com>
2025-06-05 12:35:05 +0200

    Use PROJECT_DIR variable for Apache config

--- docker_controller/entrypoint.sh
+++ docker_controller/entrypoint.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 set -e
 
-if [ -d "/var/www/app" ]; then
-  mkdir -p /var/www/app/var/cache /var/www/app/var/log
+envsubst '$PROJECT_DIR' < /templates/vhosts.conf > /etc/apache2/sites-enabled/000-default.conf
+
+PROJECT_PATH="/var/www/${PROJECT_DIR}"
+
+if [ -d "$PROJECT_PATH" ]; then
+  mkdir -p "$PROJECT_PATH/var/cache" "$PROJECT_PATH/var/log"
 
   # Récupère UID / GID du code monté
-  HOST_UID=$(stat -c '%u' /var/www/app)
-  HOST_GID=$(stat -c '%g' /var/www/app)
+  HOST_UID=$(stat -c '%u' "$PROJECT_PATH")
+  HOST_GID=$(stat -c '%g' "$PROJECT_PATH")
 
   # ACL : www-data + UID numérique + groupe numérique
   setfacl -R -m u:www-data:rwX \
               -m u:${HOST_UID}:rwX \
               -m g:${HOST_GID}:rwX \
-              /var/www/app/var
+              "$PROJECT_PATH/var"
 
   setfacl -R -d -m u:www-data:rwX \
                 -m u:${HOST_UID}:rwX \
                 -m g:${HOST_GID}:rwX \
-                /var/www/app/var
+                "$PROJECT_PATH/var"
 fi
 
 exec "$@"
+
